1ef1119f715207bef318181aa238b53d3f133c43
Ethan <minimusubi@users.noreply.github.com>
2022-07-25 01:05:10 -1000

    chore: add GitHub actions to deploy release

--- prepare-package.js
+++ prepare-package.js
@@ -1,11 +1,31 @@
 import {cp, readFile, writeFile} from 'fs/promises';
 
-const packageJson = JSON.parse(await readFile('package.json'));
+const [, , nextVersion] = process.argv;
+const packageJson = JSON.parse(await readFile('./package.json'));
+
+function toJsonString(object) {
+	return JSON.stringify(object, null, '  ');
+}
+
+if (nextVersion !== undefined) {
+	packageJson.version = nextVersion;
+	await writeFile('./package.json', `${toJsonString(packageJson)}\n`);
+}
+
 delete packageJson.devDependencies;
 delete packageJson.private;
 
 await Promise.all([
+	await new Promise((resolve, reject) => {
+		cp('CHANGELOG.md', './dist/CHANGELOG.md').then(resolve).catch((error) => {
+			if (error.code === 'ENOENT') {
+				resolve();
+			} else {
+				reject(error);
+			}
+		});
+	}),
 	await cp('LICENSE', './dist/LICENSE'),
-	await writeFile('./dist/package.json', JSON.stringify(packageJson)),
+	await writeFile('./dist/package.json', `${toJsonString(packageJson)}\n`),
 	await cp('README.md', './dist/README.md'),
 ]);
